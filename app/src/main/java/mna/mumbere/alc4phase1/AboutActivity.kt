6ee8b6ec0f04238e4d98c9668513835b70a3357a
Mumbere Arnold <arnoldnzibuk@gmail.com>
2019-07-15 17:27:40 +0300

    add webView url

--- app/src/main/java/mna/mumbere/alc4phase1/AboutActivity.kt
+++ app/src/main/java/mna/mumbere/alc4phase1/AboutActivity.kt
@@ -3,6 +3,7 @@ package mna.mumbere.alc4phase1
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class AboutActivity : AppCompatActivity() {
 
@@ -10,7 +11,18 @@ class AboutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
 
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+
         val myWebView: WebView = findViewById(R.id.webView)
+        myWebView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+        }
         myWebView.loadUrl("https://andela.com/alc/")
+
+
     }
 }
